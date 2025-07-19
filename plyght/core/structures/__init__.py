08064f3aec83555b24033d64aa3becb26a9ed232
parkermmr <parkerrennie@outlook.com>
2025-07-19 11:38:00 +1000

    feat(...): Adds state structure to core.

---
+++ plyght/core/structures/__init__.py
@@ -0,0 +1,4 @@
+from .state import State
+
+
+__all__ = ["State"]
--- plyght/core/structures/state.py
+++ plyght/core/structures/state.py
@@ -1,2 +1,27 @@
+from typing import Any
+
+
 class State:
-    pass
+    """
+    An object that can be used to store arbitrary state.
+    """
+
+    _state: dict[str, Any]
+
+    def __init__(self, state: dict[str, Any] | None = None):
+        if state is None:
+            state = {}
+        super().__setattr__("_state", state)
+
+    def __setattr__(self, key: Any, value: Any) -> None:
+        self._state[key] = value
+
+    def __getattr__(self, key: Any) -> Any:
+        try:
+            return self._state[key]
+        except KeyError:
+            message = "'{}' object has no attribute '{}'"
+            raise AttributeError(message.format(self.__class__.__name__, key))
+
+    def __delattr__(self, key: Any) -> None:
+        del self._state[key]
